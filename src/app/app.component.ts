f053083e32ad408e9700d3a7406967aa542918b5
carmineiannotti <carmineiannotti@gmail.com>
2020-12-08 22:31:46 +0100

    added permission at homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cognitive-Application';
+
+  constructor() {
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(function(stream) {
+        console.log('You let me use your mic!')
+      })
+      .catch(function(err) {
+        console.log('No mic for you!')
+      });
+
+  }
 }
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,40 @@ import { Router} from '@angular/router';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+
 export class HomeComponent implements OnInit {
 
+
   constructor(private router: Router) {
+
   }
 
   ngOnInit(): void {
-  }
+    let button = document.getElementById('buttonLogin');
+      navigator.permissions.query(
+        { name: 'microphone' }
+      ).then((permissionStatus) => {
+        if (permissionStatus.state === 'granted'){
+          // @ts-ignore
+          button.style.display = 'block';
+        }else {
+          // @ts-ignore
+          button.style.display = 'none'
+        }
+        permissionStatus.onchange = () =>{
+          console.log(permissionStatus.state);// granted, denied, prompt
+          if (permissionStatus.state === 'granted' || permissionStatus.state === 'prompt'){
+            // @ts-ignore
+            button.style.display = 'block';
+          }else {
+            // @ts-ignore
+            button.style.display = 'none'
+          }
+        }
+      })
 
+  }
 
   goToLogin(){
     this.router.navigateByUrl("/login");
